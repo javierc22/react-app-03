4d17266d210681538e275d9342c1d791ce98ad5c
javierc22 <javier.ec.22@gmail.com>
2020-06-09 22:49:56 -0400

    Leyendo la información que el usuario selecciona

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from '@emotion/styled';
 
 const Campo = styled.div`
@@ -42,11 +42,29 @@ const Boton = styled.button`
 `;
 
 const Formulario = () => {
+
+  const [ datos, guardarDatos ] = useState({
+    marca: '',
+    year: '',
+    plan: ''
+  });
+
+  // Extraer los valores del state
+  const { marca, year, plan } = datos;
+
+  // Leer los datos del formulario y colocarlos en el state
+  const obtenerInformacion = e => {
+    guardarDatos({
+      ...datos,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <form>
       <Campo>
         <Label>Marca</Label>
-        <Select>
+        <Select name="marca" value={marca} onChange={obtenerInformacion}>
           <option value="">-- Seleccione --</option>
           <option value="americano">Americano</option>
           <option value="europeo">Europeo</option>
@@ -55,7 +73,7 @@ const Formulario = () => {
       </Campo>
       <Campo>
         <Label>Año</Label>
-        <Select>
+        <Select name="year" value={year} onChange={obtenerInformacion}>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -71,8 +89,20 @@ const Formulario = () => {
       </Campo>
       <Campo>
         <Label>Plan</Label>
-        <InputRadio type="radio" name="plan" value="basico" /> Básico
-        <InputRadio type="radio" name="plan" value="completo" /> Completo
+        <InputRadio 
+          type="radio"
+          name="plan"
+          value="basico"
+          checked={plan === "basico"}
+          onChange={obtenerInformacion}
+        /> Básico
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="completo"
+          checked={plan === "completo"}
+          onChange={obtenerInformacion}
+        /> Completo
       </Campo>
       <Boton type="button">Cotizar</Boton>
     </form>
