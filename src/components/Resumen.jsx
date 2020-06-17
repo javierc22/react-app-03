7f3825ede433b200ded4192fc72d3e5935a86dea
javierc22 <javier.ec.22@gmail.com>
2020-06-16 21:52:13 -0400

    Mostrando el resumen

--- src/components/Resumen.jsx
+++ src/components/Resumen.jsx
@@ -1,4 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import styled from '@emotion/styled';
+import { primeraMayuscula } from '../helper';
+
+const ContenedorResumen = styled.div`
+  padding: 1rem;
+  text-align: center;
+  background-color: #00838F;
+  color: #FFF;
+  margin-top: 1rem;
+`;
 
 const Resumen = ({ datos }) => {
   // extraer de datos
@@ -7,14 +17,14 @@ const Resumen = ({ datos }) => {
   if (marca === '' || year === '' || plan === '') return null;
 
   return (
-    <Fragment>
+    <ContenedorResumen>
       <h2>Resumen de Cotización</h2>
       <ul>
-        <li>Marca: </li>
-        <li>Plan: </li>
-        <li>Año del Auto: </li>
+        <li>Marca: {primeraMayuscula(marca)}</li>
+        <li>Plan: {primeraMayuscula(plan)}</li>
+        <li>Año del Auto: {primeraMayuscula(year)}</li>
       </ul>
-    </Fragment>
+    </ContenedorResumen>
   );
 }
  
