ec7546355acf7f095de0e1d3a94b76a26b5d3fc8
javierc22 <javier.ec.22@gmail.com>
2020-06-16 20:55:12 -0400

    Calculando el costo basado en años

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from '@emotion/styled';
+import { obtenerDiferenciaYear } from '../helper';
 
 const Campo = styled.div`
   display: flex;
@@ -82,11 +83,16 @@ const Formulario = () => {
 
     guardarError(false);
 
+    // una base de 2000
+    let resultado = 2000;
+
     // Obtener la diferencia de años
+    const diferencia = obtenerDiferenciaYear(year);
 
     // por cada año hay que restar el 3%
+    resultado -= ((diferencia * 3) * resultado) / 100;
 
-    
+
   }
 
   return (
